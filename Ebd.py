d855fbf3a61922f958d33fd8ce8698635672dcaa
DyeisonCS <151203149+DyeisonCS@users.noreply.github.com>
2026-01-10 15:06:12 -0400

    Add files via upload

--- Ebd.py
+++ Ebd.py
@@ -1,19 +1,33 @@
 import streamlit as st
 import pandas as pd
 from bd import funcoes
+from supabase import create_client, Client
 
-st.write('Cadastro de Aluno Escola Biblica Dominical')
+url = "https://vncdlatpuvniwwzdebtb.supabase.co"
+key = "sb_publishable_X5t9KZPR-0LJZF0BaMRT5w_rmhCUW6h"
+supabase: Client = create_client(url, key)
+
+
+st.title('Cadastro de Aluno Escola Biblica Dominical') #write
 
 nome = st.text_input('Nome')
 data_nasc = st.text_input('data_nasc')
 classe = st.text_input('classe')
 
 if st.button('Adicionar Aluno'):
-    funcoes.inseredados(nome, data_nasc, classe)
+    data = {
+        'nome': nome,
+        'data_nasc': data_nasc,
+        'classe': classe
+    }
+    supabase.table('alunos').insert(data).execute()
+    #funcoes.inseredados(nome, data_nasc, classe)
     st.success('Adicionado')
 
-if st.button('listar clientes'):
-    dados = funcoes.listardados()
-    tb = pd.DataFrame(dados, columns=['ID', 'nome', 'data_nasc', 'classe'])
-    st.header('Alunos')
-    st.table(tb)
+if st.button('Listar Alunos'):
+    #dados = funcoes.listardados()
+    #tb = pd.DataFrame(dados, columns=['ID', 'nome', 'data_nasc', 'classe'])
+    res = supabase.table('alunos').select('*').execute()
+    st.write('Alunos:')   #header
+    st.table(res.data)    
+    #st.table(tb)  
