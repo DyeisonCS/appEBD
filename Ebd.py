5006156c1dabf6b1794035c47ceebc0d9ed60a65
DyeisonCS <151203149+DyeisonCS@users.noreply.github.com>
2026-01-11 11:19:42 -0400

    Update Ebd.py

--- Ebd.py
+++ Ebd.py
@@ -1,17 +1,7 @@
 import streamlit as st
 import pandas as pd
 from bd import funcoes
-from supabase import create_client, Client
 from bd.database import database_segment
-
-#url = os.environ["SUPABASE_URL"]
-#key = os.environ["SUPABASE_KEY"]
-#supabase = create_client(url, key)
-
-#url = "https://vncdlatpuvniwwzdebtb.supabase.co"
-#key = "sb_publishable_X5t9KZPR-0LJZF0BaMRT5w_rmhCUW6h"
-#supabase: Client = create_client(url, key)
-
 
 st.set_page_config(
     page_icon= 'logo_ipb.png',
@@ -19,5 +9,6 @@ st.set_page_config(
 )
 
 st.header('Escola Bíblica Dominical IPSO', text_alignment= "center") #write
+
 
 database_segment()
