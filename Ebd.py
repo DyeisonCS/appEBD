cd70f137878bc2971baa3bda36a3c6ff1625706b
DyeisonCS <151203149+DyeisonCS@users.noreply.github.com>
2026-01-13 11:55:08 -0400

    Update Ebd.py

--- Ebd.py
+++ Ebd.py
@@ -5,12 +5,12 @@ from bd.database import database_segment
 
 st.set_page_config(
     page_icon= 'logo_ipb.png',
-    page_title= 'EBD IPSO',
-    primaryColor = '#4CAf50'
+    page_title= 'EBD IPSO'
 )
 
 st.header('Escola Bíblica Dominical IPSO', text_alignment= "center") #write
 
 
 database_segment()
+
 
