50b8f801979c46c36dc668c884b606e760ff78f4
DyeisonCS <151203149+DyeisonCS@users.noreply.github.com>
2026-01-12 21:05:31 -0400

    Update database.py

--- bd/database.py
+++ bd/database.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from supabase import create_client, Client
+import pandas as pd
 import os
 import datetime
 
@@ -11,8 +12,8 @@ supabase = create_client(url, key)
 
 def database_segment():
     #st.header("🗄 Database", help="Testando criação de abas e paginas.")
-    lista_alunos, cadastrar, drop_tab, describe_tab, show_tab, ANIVERSARIANTES, tips_tab = \
-        st.tabs(["Listar Alunos", "Cadastrar", "DROP", "DESCRIBE", "SHOW", "ANIVERSARIANTES", "❄️"])
+    lista_alunos, cadastrar, turmas, ANIVERSARIANTES = \
+        st.tabs(["Listar Alunos", "Cadastrar", "Turmas", "ANIVERSARIANTES"])
     
     with lista_alunos:
         if st.button('Listar Alunos'):
@@ -25,7 +26,7 @@ def database_segment():
 
     with cadastrar:
         nome = st.text_input('Nome')
-        data_nasc = st.date_input('Data Nascimento', min_value=datetime.date(1910, 1, 1), format="DD/MM/YYYY").strftime("%d/%m/%Y")
+        data_nasc = st.date_input('Data Nascimento', datetime.date(1910, 1, 1), format="DD/MM/YYYY").strftime("%d/%m/%Y")
         classe = st.selectbox('Turma', options=['Vencedores por Cristos', 'Soldados de Cristo', 'Jardim de Deus', 'Joias de Cristo'], index=None, placeholder='Selecione')
         sexo = st.selectbox('Sexo', options=['masculino', 'feminino'], index=None, placeholder='Selecione')
 
@@ -41,31 +42,15 @@ def database_segment():
 
 
 
-    with drop_tab:
-        st.button('Teste Drop')
+    with turmas:
+        res = supabase.table('alunos').select('nome', 'classe').execute()
+        st.write('Turmas:')             #header
+        df = pd.DataFrame(res.data)     #criando df
+        df['presenca'] = False          #criando coluna de presença com todos pendente de presença
+        st.data_editor(df)    
+        #st.table(tb)
 
-    with describe_tab:
-        st.button('Teste Describe')
-
-    with show_tab:
-        st.button('Teste Show')
-    
     with ANIVERSARIANTES:
         st.tabs(["Dyeison", "Rafael"])
-
-    with tips_tab:
     
-        st.markdown("""
-        💡 **Tips**
-        - Follow consistent and meaningful naming conventions for `DATABASE` objects.
-        - When you create a new Snowflake database, it also generates two schemas: `PUBLIC` (the default schema) and `INFORMATION_SCHEMA` (containing views and table functions for querying metadata across objects).
-        - Use a `TRANSIENT` `DATABASE` to isolate temporary data, and provide a dedicated space for intermediate results or temporary tables during specific analysis or transformation tasks.
-        - Utilize zero-copy cloning using `CREATE DATABASE <name> CLONE <source_db>` for efficient, space-saving `DATABASE` copies.
-        - Continuously analyze query and resource usage patterns to fine-tune `DATABASE` parameters for optimal performance and cost efficiency.
-        """
-        )
-    
-
-
-
 
