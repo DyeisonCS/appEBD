64a9d93f2021b58d4905e05ebb0a7e302d0bad06
DyeisonCS <151203149+DyeisonCS@users.noreply.github.com>
2026-01-22 22:58:24 -0400

    Update database.py

--- bd/database.py
+++ bd/database.py
@@ -5,9 +5,12 @@ import pandas as pd
 import os
 import datetime
 from bd.banco import PresencaData
+import locale
 from utils.styles import inject_mobile_css
 
 inject_mobile_css()
+
+locale.setlocale(locale.LC_TIME, "pt_BR.UTF-8")
 
 url = os.environ["SUPABASE_URL"]
 key = os.environ["SUPABASE_KEY"]
@@ -17,45 +20,45 @@ supabase = create_client(url, key)
 def database_segment():
     inject_mobile_css()
     #st.header("🗄 Database", help="Testando criação de abas e paginas.")
-    lista_alunos, ANIVERSARIANTES = \
-        st.tabs(["Listar Alunos", "ANIVERSARIANTES"])
+    Resumo, ANIVERSARIANTES = \
+        st.tabs(["Resumo", "ANIVERSARIANTES"])
     
-    with lista_alunos:
-        diafiltro = st.date_input('Data Para Relatórios', min_value= datetime.date(2026, 1, 1), format="DD/MM/YYYY")
-        res = PresencaData(diafiltro)
-        tabela = pd.DataFrame(res)
-        totalalunospresentes = len(tabela)
+    with Resumo:
+        #diafiltro = st.date_input('Data Nascimento', min_value= datetime.date(2026, 1, 1), format="DD/MM/YYYY")
+        res = PresencaData()
+        tabelacompleta = pd.DataFrame(res)
+        totalalunospresentes = len(tabelacompleta)
 
         if totalalunospresentes > 0:
-            lista_unica = list(tabela['classe'].unique())
-            if len(tabela) > 0:
-                for turma in lista_unica:
-                    #st.write(f'{turma}:')   #header
-                    #st.table(res.data) 
-                    #tabela = pd.DataFrame(res.data)
-                    tabelafor = tabela[['nome', 'presenca', 'classe']].sort_values(by="nome").reset_index(drop=True)
-                    tabelafiltrada = tabelafor[tabelafor['classe'] == turma]
+            lista_unica = list(tabelacompleta['data'].unique())
 
-                    presentes = len(tabelafiltrada[tabelafiltrada['presenca'] == True])
-                    progresso = presentes / len(tabelafiltrada)
-                    text_progresso = f'{progresso * 100: .2f}%'
-                    st.progress(progresso, text= f'{turma} {text_progresso}')
+            for dia in lista_unica:
+                datafor = datetime.date.fromisoformat(dia)
+                datadia = datafor.strftime('%A')
+                tabelacompleta["data"] = pd.to_datetime(tabelacompleta["data"]).dt.date
+                tabela = tabelacompleta[tabelacompleta['data'] == datafor]
 
-        #if st.button('Listar Alunos'):
-        #    #dados = funcoes.listardados()
-        #    #tb = pd.DataFrame(dados, columns=['ID', 'nome', 'data_nasc', 'classe'])
-        #    res = supabase.table('alunos').select('nome', 'classe').execute()
-        #    st.write('Alunos:')   #header
-        #    st.table(res.data)    
-        #    #st.table(tb)
+                presentes = tabela['presenca'].sum()
+                #st.data_editor(tabela, key=dia)
 
+                with st.expander(f'📆 {datafor.strftime("%d/%m/%Y")} {datadia}:'): 
+                    #st.write(presentes)
+                    lista_unica = list(tabela['classe'].unique())
+                    for turma in lista_unica:
+                        tabelafor = tabela[['nome', 'presenca', 'classe']].sort_values(by="nome").reset_index(drop=True)
+                        tabelafiltrada = tabelafor[tabelafor['classe'] == turma]
 
+                        presentes = len(tabelafiltrada[tabelafiltrada['presenca'] == True])
+                        progresso = presentes / len(tabelafiltrada)
+                        text_progresso = f'{progresso * 100: .2f}%'
+                        st.progress(progresso, text= f'{turma} {text_progresso}')
     
 
 
     with ANIVERSARIANTES:
         st.tabs(["Dyeison", "Rafael"])
     
+
 
 
 
